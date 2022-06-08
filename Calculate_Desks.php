3ac0eef8c02152f2fd142861ae399972e532ad51
Habiba-2002 <100557019+Habiba-2002@users.noreply.github.com>
2022-06-08 09:38:08 +0200

    Create Calculate_Desks.php

---
+++ Calculate_Desks.php
@@ -0,0 +1,30 @@
+<?php
+
+include_once "iCalculate.php";
+include_once "FileManager.php";
+class Needed_Desks implements Calculate
+{
+    private $FileManagerObj; 
+    function __construct()
+    {
+        $this->FileManagerObj= new FileManager();
+        $this->FileManagerObj->setFilename("Orphan.txt");
+        $this->FileManagerObj->setSeparator("~");
+    }
+	function Calculate_Needed($Amount) 
+    {
+        $total_Or=$this->getFileManagerObj()->Count_File();
+        $Needed=$total_Or-$Amount;
+        return $Needed;
+    }
+	function getFileManagerObj()
+    {
+		return $this->FileManagerObj;
+	}
+    function setFileManagerObj($FileManagerObj): self 
+    {
+		$this->FileManagerObj = $FileManagerObj;
+		return $this;
+	}
+}
+?>
